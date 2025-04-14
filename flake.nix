ea6d9555a664f2b31fadd377d5797bc8db4fe17c
maningreen <flibbers@protonmail.com>
2025-04-13 20:28:50 -0600

    feat: flake supports all arches

--- flake.nix
+++ flake.nix
@@ -6,11 +6,24 @@
   };
 
   outputs = { self , nixpkgs ,... }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    supportedSystems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
-    devShells.${system}.default = pkgs.callPackage ./shell.nix { inherit pkgs; };
+    devShells = forAllSystems (system: let 
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.callPackage ./shell.nix { inherit pkgs; };
+    });
 
-    packages.${system}.default = pkgs.callPackage ./default.nix { inherit pkgs; };
+    packages = forAllSystems (system: let 
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.callPackage ./default.nix { inherit pkgs; };
+    });
   };
 }
