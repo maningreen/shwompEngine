ab715550a435b6209032df970c4c49ef52b9aa95
maningreen <flibbers@protonmail.com>
2025-10-09 21:37:12 -0600

    removed unsupported systems from supported systems...

--- flake.nix
+++ flake.nix
@@ -5,25 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self , nixpkgs ,... }: let
+  outputs = {
+    nixpkgs,
+    ...
+  }: let
     supportedSystems = [
       "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
     ];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
-    devShells = forAllSystems (system: let 
+    devShells = forAllSystems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      default = pkgs.callPackage ./shell.nix { inherit pkgs; };
+      default = pkgs.callPackage ./shell.nix {inherit pkgs;};
     });
 
-    packages = forAllSystems (system: let 
+    packages = forAllSystems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      default = pkgs.callPackage ./default.nix { inherit pkgs; };
+      default = pkgs.callPackage ./default.nix {inherit pkgs;};
     });
   };
 }
